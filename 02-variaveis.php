e8c6f122978addb4da6e0b33b69a02854dcc081d
Isameidabebel2 <102260142+Isameidabebel2@users.noreply.github.com>
2022-04-09 16:54:04 -0300

    Add files via upload

---
+++ 02-variaveis.php
@@ -0,0 +1,32 @@
+<?php
+    //Meus dados
+    $nome="Isabel Almeida";
+    $idade=17;
+    $altura=1.67;
+    echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura<br>";
+
+    //Alterando meus dados
+    $nome="Felipe";
+    $idade=29;
+    $altura=1.90;
+    echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura<br>";
+    echo "<hr>";
+
+    $carro="Hilux";
+    $Carro="Fusca";
+    echo "$Carro<br>";
+    echo $carro;
+
+    //Nomes válidos
+    //$nome
+    //$_meunome
+    //$carro10
+    //$nota1
+    //$meu_nome
+    //$meuNome
+
+    //Nomes inválidos
+    //$123456nome
+    //$carro*&!@#
+    //$meu carro
+?>
---
+++ 03-variaveis-variaveis.php
@@ -0,0 +1,20 @@
+<?php
+
+/*Isso é a partir do php 7.0
+
+//Variáveis variáveis
+
+$bebida="refrigerante ";
+$$bebida="Guaraná";
+
+echo $refrigerante;
+
+echo "<br>";
+
+$destino=""cidade;
+$$destino="Ilhéus";
+
+echo $cidade;
+*/
+
+?>
